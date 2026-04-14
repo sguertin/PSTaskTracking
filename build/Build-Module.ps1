2a5045de4060ecddfeb885fd1ee506adcca2399d
Scott Guertin <sguertin@gmail.com>
2026-04-14 12:02:43 -0400

    Updated .gitignore for build, added build settings

--- build/Build-Module.ps1
+++ build/Build-Module.ps1
@@ -10,11 +10,16 @@ while (!$sourceDirectory.ToString().EndsWith("PSTaskTracking")) {
     }    
 }
 if ($continue) {
+    $settings = Get-Content (Join-Path "build" -ChildPath "build.json" ) -Raw | ConvertFrom-Json;
     $aliasRegex = "Set-Alias -Name (.*?) -Value .*"
-    $outputDirectory = Join-Path $PWD -ChildPath "output";
+    $outputDirectory = Join-Path $PWD -ChildPath "PSTaskTracking";
     $content = ""; 
     $functions = @();
     $aliases = @();
+    if (Test-Path $outputDirectory) {        
+        Remove-Item $outputDirectory -Recurse -Force;
+    }
+    New-Item $outputDirectory -ItemType Directory -Force | Out-Null;
     foreach ($file in Get-ChildItem -Path $sourceDirectory -File -Filter "*.ps1" | Where-Object Name -NE "Update-Module.ps1") {
         $functionContent = Get-Content $file -Raw; 
         $content += "$functionContent`n"; 
@@ -23,11 +28,15 @@ if ($continue) {
             $aliases += $match.Groups[1].Value
         }
     } 
-    $moduleContent = Get-Content (Join-Path ".build" -ChildPath "PSTaskTracking.psm1") -Raw;
+    $moduleContent = Get-Content (Join-Path "build" -ChildPath "PSTaskTracking.psm1") -Raw;
     $moduleContent = $moduleContent.Replace("#{ModuleContent}#", $content);
     $moduleOutputPath = Join-Path $outputDirectory -ChildPath "PSTaskTracking.psm1";
     $manifestOutputPath = Join-Path $outputDirectory -ChildPath "PSTaskTracking.psd1";
-    Copy-Item (Join-Path ".build" -ChildPath "PSTaskTracking.psd1") -Destination $manifestOutputPath -Force;
-    Update-ModuleManifest -Path $manifestOutputPath -FunctionsToExport $functions -AliasesToExport $aliases;
-    New-Item -Path $moduleOutputPath -ItemType File -Value $moduleContent -Force;    
+    $User = $env:USERNAME;
+    if ([string]::IsNullOrEmpty($User)) {
+        $User = $env:USER;
+    }
+    New-Item -Path $moduleOutputPath -ItemType File -Value $moduleContent -Force | Out-Null;
+    New-ModuleManifest -Path $manifestOutputPath -Guid $settings.ProjectId -ModuleVersion '2.0';
+    Update-ModuleManifest -Path $manifestOutputPath -RootModule "PSTaskTracking.psm1" -FunctionsToExport $functions -AliasesToExport $aliases;
 }
