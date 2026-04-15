c3ce88d991d5140bb5d4d488b3c9ca7c0aa295c6
Scott Guertin <sguertin@gmail.com>
2026-04-15 12:20:36 -0400

    Bump build version and made it part of build.json

--- build/Build-Module.ps1
+++ build/Build-Module.ps1
@@ -5,32 +5,37 @@ while (!$sourceDirectory.ToString().EndsWith("PSTaskTracking")) {
     if ($parentDirectory.ToString() -eq $sourceDirectory.ToString()) {
         Write-Error "Unable to find PSTaskTracking directory!"
         $continue = $false;
-    } else {
+    }
+    else {
         $sourceDirectory = $parentDirectory;
-    }    
+    }
 }
 if ($continue) {
     $settings = Get-Content (Join-Path "build" -ChildPath "build.json" ) -Raw | ConvertFrom-Json;
-    $aliasRegex = "Set-Alias -Name (.*?) -Value .*"
-    $outputDirectory = Join-Path $PWD -ChildPath "PSTaskTracking";
-    $content = ""; 
+    $outputDirectory = Join-Path $PWD -ChildPath "Module" -AdditionalChildPath @("PSTaskTracking");
+    $archiveFilePath = Join-Path $PWD -ChildPath "PSTaskTracking";
+    $content = "";
     $functions = @();
     $aliases = @();
-    if (Test-Path $outputDirectory) {        
+    if (Test-Path $outputDirectory) {
         Remove-Item $outputDirectory -Recurse -Force;
     }
+
+    if (Test-Path "$archiveFilePath.zip") {
+        Remove-Item "$archiveFilePath.zip" -Force;
+    }
     New-Item $outputDirectory -ItemType Directory -Force | Out-Null;
-    foreach ($file in Get-ChildItem -Path $sourceDirectory -File -Filter "*.ps1" | Where-Object Name -NE "Update-Module.ps1") {
-        $functionContent = Get-Content $file -Raw; 
+    foreach ($file in Get-ChildItem -Path $sourceDirectory -File -Filter "*.ps1") {
+        $functionContent = Get-Content $file -Raw;
         $content += "$functionContent`n`n";
         $function = $file.Name.Replace(".ps1", "");
         if (!$settings.FunctionsToExclude.Contains($function)) {
             $functions += $function;
-        }        
-        foreach ($match in ([regex]$aliasRegex).Matches($functionContent)) {
+        }
+        foreach ($match in ([regex]"Set-Alias -Name (.*?) -Value .*").Matches($functionContent)) {
             $aliases += $match.Groups[1].Value
         }
-    } 
+    }
     $moduleContent = Get-Content (Join-Path "build" -ChildPath "PSTaskTracking.psm1") -Raw;
     $moduleContent = $moduleContent.Replace("#{ModuleContent}#", $content);
     $moduleOutputPath = Join-Path $outputDirectory -ChildPath "PSTaskTracking.psm1";
@@ -40,8 +45,7 @@ if ($continue) {
         $User = $env:USER;
     }
     New-Item -Path $moduleOutputPath -ItemType File -Value $moduleContent -Force | Out-Null;
-    New-ModuleManifest -Path $manifestOutputPath -Guid $settings.ProjectId -ModuleVersion '2.0';
+    New-ModuleManifest -Path $manifestOutputPath -Guid $settings.ProjectId -ModuleVersion $settings.ModuleVersion;
     Update-ModuleManifest -Path $manifestOutputPath -RootModule "PSTaskTracking.psm1" -FunctionsToExport $functions -AliasesToExport $aliases;
-    $archiveFilePath = Join-Path $PWD -ChildPath "PSTaskTracking";
     Compress-Archive -Path $outputDirectory -CompressionLevel Optimal -DestinationPath $archiveFilePath;
 }
