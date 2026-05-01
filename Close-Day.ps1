42786d6aaf53de6aed21e508776ffe20ba422fac
Scott Guertin <sguertin@gmail.com>
2026-04-30 21:52:13 -0400

    Update so the summary markdown file gets sent to the output directory if no conversion function is specified

--- Close-Day.ps1
+++ Close-Day.ps1
@@ -19,22 +19,26 @@ function Close-Day {
     if ($null -eq $reportFile) {
         return;
     }
-    $reportFileName = $reportFile.FullName;
-    & $script:Settings.Editor $reportFileName;
+    $reportFilePath = $reportFile.FullName;
+    $reportFileName = $reportFile.Name;
+    & $script:Settings.Editor $reportFilePath;
+    $mdToPdfCmd = $script:Settings.MarkdownToPdfCommand;
     
-    if ([string]::IsNullOrEmpty($script:Settings.MarkdownToPdfCommand)) {        
-        return;
-    }
-    if ([string]::IsNullOrEmpty($script:Settings.OutputDirectory)) {
+    if ([string]::IsNullOrEmpty($script:Settings.OutputDirectory)) {        
         $outputDirectory = Get-TaskFolder;
     } else {
         $outputDirectory = $script:Settings.OutputDirectory;
     }
-    $outputFileName = $reportFile.Name.Replace(".md", ".pdf");
-    $outputFilePath = Join-Path -Path $outputDirectory -ChildPath $outputFileName;
-    $outputCommand = $script:Settings.MarkdownToPdfCommand.Replace("#{input}#", "`"$reportFileName`"").Replace("#{output}#", "`"$outputFilePath`"");
-    Write-Host "> $outputCommand";
-    Invoke-Expression -Command $outputCommand | Out-Null;
-    Write-Host "$outputFileName written to '$outputDirectory'";
+    if ([string]::IsNullOrEmpty($mdToPdfCmd)) {
+        Write-Host "COPY $reportFileName ====> $outputDirectory";
+        Copy-Item -$reportFilePath -Destination (Join-Path $outputDirectory -ChildPath $reportFileName) -Force;
+    } else {
+        $outputFileName = $reportFileName.Replace(".md", ".pdf");
+        $outputFilePath = Join-Path -Path $outputDirectory -ChildPath $outputFileName;
+        $outputCommand = $mdToPdfCmd.Replace("#{input}#", "`"$reportFilePath`"").Replace("#{output}#", "`"$outputFilePath`"");
+        Write-Host "> $outputCommand";
+        Invoke-Expression -Command $outputCommand | Out-Null;
+        Write-Host "$outputFileName written to '$outputDirectory'";
+    }    
 }
 Set-Alias -Name CloseDay -Value Close-Day;
