2bda2d342091636f7227b2e6728bba46019c915e
Scott Guertin <sguertin@gmail.com>
2026-05-02 00:32:31 -0400

    Set additional app constants

--- build/PSTaskTracking.psm1
+++ build/PSTaskTracking.psm1
@@ -1,5 +1,16 @@
+$script:AppName = "PSTaskTracking";
+$script:DATE_FORMAT = "yyyy-MM-dd";
+$script:DATETIME_FORMAT = "dddd M/d/yyyy h:m tt";
+
 #{ModuleContent}#
 
+if ([string]::IsNullOrEmpty($env:LOCALAPPDATA)) {
+    $script:AppFolder = Join-Path -Path $env:HOME -ChildPath ".local" -AdditionalChildPath @($script:AppName);
+} else {
+    $script:AppFolder = Join-Path ($env:LOCALAPPDATA) -ChildPath $script:AppName;
+}
+
+$script:AppFolder = Join-Path ($appFolderPath) -ChildPath $script:AppName;
 $script:Settings = Get-TaskTrackerSettings;
 
 Initialize-PSTaskTracking;
