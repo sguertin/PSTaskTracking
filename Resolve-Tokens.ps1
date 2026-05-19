abc563da1b34a35a4caf79346cc50fed19373a8e
Scott Guertin <sguertin@gmail.com>
2026-05-19 18:14:42 -0400

    Add string rendering function

---
+++ Resolve-Tokens.ps1
@@ -0,0 +1,25 @@
+function Resolve-Tokens {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline = $true)]
+        [string]$Content,        
+        [Parameter(Mandatory, Position = 1)]
+        [string]$Token,
+        [Parameter(Mandatory, Position = 2)]
+        [string]$Value,
+        [hashtable]$Values = $null
+    )
+    begin {
+        $tokenValue = "#{$Token}#";
+    }
+    process {
+        if ($null -ne $Values) {
+            foreach ($key in $Values.keys) {
+                $value = $Values[$key];
+                $Content = $Content.Replace("#{$key}#", $value);
+            }
+        }
+        return $Content.Replace($tokenValue, $Value);
+    }
+
+}
